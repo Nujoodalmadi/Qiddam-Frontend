ea4f195d2e4bc73650d36a9bccb8d74dd98814a3
Nadya <nadeia.ak@gmail.com>
2019-05-01 07:09:40 +0300

    update

--- components/UpdateProfile/index.js
+++ components/UpdateProfile/index.js
@@ -1,36 +1,42 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { TextInput, TouchableOpacity, ScrollView } from "react-native";
+import * as actionCreators from "../../store/actions";
+import {
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  View,
+  TextInput,
+  Text
+} from "react-native";
+import { Form } from "native-base";
+import styles from "../UpdateProfile/style";
 
 class UpdateProfile extends Component {
   state = {
-    user: { first_name: "", last_name: "", email: "" },
-    img: "",
+    first_name: "",
+    last_name: "",
+    email: "",
     bio: ""
   };
   componentDidMount() {
-    //this.props.profile &&
     this.setState({
-      user: {
-        first_name: this.props.profile.user.first_name,
-        last_name: this.props.profile.user.last_name,
-        email: this.props.profile.user.email
-      },
-      img: this.props.profile.img,
+      first_name: this.props.profile.user.first_name,
+      last_name: this.props.profile.user.last_name,
+      email: this.props.profile.user.email,
       bio: this.props.profile.bio
     });
   }
 
   componentDidUpdate(prevState) {
-    this.setState({
-      user: {
+    if (this.state === prevState) {
+      this.setState({
         first_name: this.props.profile.user.first_name,
         last_name: this.props.profile.user.last_name,
-        email: this.props.profile.user.email
-      },
-      img: this.props.profile.img,
-      bio: this.props.profile.bio
-    });
+        email: this.props.profile.user.email,
+        bio: this.props.profile.bio
+      });
+    }
   }
 
   static navigationOptions = {
@@ -40,49 +46,73 @@ class UpdateProfile extends Component {
   render() {
     return (
       <ScrollView>
-        <TextInput
-          //   style={styles.inputs}
-          placeholder="اسمك الاول"
-          value={this.state.first_name}
-          autoCapitalize="none"
-          onChangeText={first_name => this.setState({ first_name })}
-        />
-        <TextInput
-          //   style={styles.inputs}
-          placeholder="اسمك الاخير"
-          value={this.state.last_name}
-          autoCapitalize="none"
-          onChangeText={last_name => this.setState({ last_name })}
-        />
-        <TextInput
-          //   style={styles.inputs}
-          placeholder="بريدك الالكتروني"
-          value={this.state.email}
-          autoCapitalize="none"
-          onChangeText={email => this.setState({ email })}
-        />
-        <TextInput
-          //   style={styles.inputs}
-          placeholder="bio"
-          value={this.state.bio}
-          autoCapitalize="none"
-          onChangeText={bio => this.setState({ bio })}
-        />
-        <TouchableOpacity
-        //   style={[styles.buttonContainer, styles.loginButton]}
-        //   onPress={() => this.props.(this.state)}
-        >
-          {/* <Text style={styles.loginText}>تسجيل دخول</Text> */}
-        </TouchableOpacity>
+        <View>
+          <View style={styles.header}>
+            <Image
+              style={{ width: 420, height: 200 }}
+              source={require("../../img/header2.png")}
+            />
+          </View>
+        </View>
+        <View style={styles.body}>
+          <Form style={styles.form}>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.inputs}
+                value={this.state.first_name}
+                autoCapitalize="none"
+                onChangeText={first_name => this.setState({ first_name })}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                // style={styles.inputs}
+
+                value={this.state.last_name}
+                autoCapitalize="none"
+                onChangeText={last_name => this.setState({ last_name })}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                value={this.state.email}
+                autoCapitalize="none"
+                onChangeText={email => this.setState({ email })}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                value={this.state.bio}
+                autoCapitalize="none"
+                onChangeText={bio => this.setState({ bio })}
+              />
+            </View>
+
+            <TouchableOpacity
+              style={[styles.buttonContainer, styles.updateButton]}
+              onPress={() =>
+                this.props.UpdateProfile(this.state, this.props.navigation)
+              }
+            >
+              <Text tyle={styles.updateButtonText}>update</Text>
+            </TouchableOpacity>
+          </Form>
+        </View>
       </ScrollView>
     );
   }
 }
 const mapStateToProps = state => {
-  return {};
+  return {
+    profile: state.authReducer.myprofile
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-  return {};
-};
-export default connect(mapStateToProps)(UpdateProfile);
+const mapDispatchToProps = dispatch => ({
+  UpdateProfile: (userData, navigation) =>
+    dispatch(actionCreators.updateProfile(userData, navigation))
+});
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UpdateProfile);
---
+++ components/UpdateProfile/styles.js
@@ -0,0 +1,135 @@
+import { StyleSheet } from "react-native";
+
+const resizeMode = "cover";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgb(184, 224, 224)"
+  },
+  inputContainer: {
+    borderBottomColor: "#F5FCFF",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 300,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center",
+
+    shadowColor: "#808080",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: "#FFFFFF",
+    flex: 1
+  },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginRight: 15,
+    justifyContent: "center"
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 100,
+    borderRadius: 30,
+    backgroundColor: "transparent"
+  },
+
+  loginButton: {
+    backgroundColor: "rgb(217,102,61)",
+
+    shadowColor: "#808080",
+    shadowOffset: {
+      width: 0,
+      height: 1
+    }
+  },
+  loginText: {
+    color: "white"
+  },
+  bgImage: {
+    flex: 1,
+    resizeMode,
+
+    width: "100%",
+    height: "100%",
+    justifyContent: "center"
+  },
+  btnText: {
+    color: "#F2CA50",
+    fontWeight: "bold"
+  },
+  textByRegister: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  gender: {
+    justifyContent: "space-around",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 30,
+    width: 100,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    shadowColor: "#808080",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5
+  },
+
+  dateIcon: {
+    position: "absolute",
+    left: 0,
+    top: 4,
+    marginLeft: 0
+  },
+  dateInput: {
+    borderWidth: 0
+  },
+  updateButton: {
+    borderRadius: 20,
+    width: 80,
+    alignSelf: "center",
+    backgroundColor: "rgba(242, 202, 80, 1)",
+    shadowColor: "#8768A6",
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22
+  },
+  updateButtonText: {
+    textAlign: "center",
+    fontSize: 15,
+    padding: 5,
+    color: "white"
+  }
+});
+
+export default styles;
